b4a124e5ec8b1cfd1752dbc6c7fe0e4c2c4cabfe
JordanRaiaNIU <z1887657@students.niu.edu>
2022-12-10 02:50:33 -0600

    Added some icons and start to main

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,96 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.grey),
+      home: const RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  bool isMute = false;
+  int beatNum = 4;
+  int beat = 1;
+  int timeSignature = 4;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Untitled'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.save),
+            color: Colors.white,
+          ),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                isMute = !isMute;
+              });
+            },
+            icon: Icon(isMute ? Icons.volume_mute : Icons.volume_up),
+            color: isMute ? Colors.teal : Colors.white,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.settings),
+            color: Colors.white,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(2.0),
+            color: Colors.cyan,
+          ),
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            color: Colors.black,
+            width: double.infinity,
+            height: (MediaQuery.of(context).size.height * 0.35) - 80,
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  for (var i = 0; i < beatNum; i++)
+                    const Icon(
+                      Icons.circle,
+                      color: Colors.cyan,
+                      size: 45,
+                    ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.black12,
+            child: Container(
+              margin: const EdgeInsets.all(5.0),
+              height: (MediaQuery.of(context).size.height * 0.20) - 80,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1
+                ),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
